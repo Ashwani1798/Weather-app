093aee40167e4e70634b866b4b1a65691d621fc5
Ashwani1798 <sood.ashwani1715@gmail.com>
2022-07-18 13:07:26 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,46 @@
+let weather = {
+    "apikey" : "a2e521ec7a249fd0c85f4af1e97824ea",
+    fetchWeather : function(city) {
+        fetch(
+            "https://api.openweathermap.org/data/2.5/weather?q="
+            + city 
+            +"&units=metric&appid="
+            +this.apikey
+        )
+        .then((response) => response.json())
+        .then((data) => this.displayWeather(data));
+    },
+    displayWeather : function(data){
+      const { name } = data;
+      const { icon, description } = data.weather[0];
+      const { temp, humidity } = data.main;
+      const { speed } = data.wind;
+      console.log(name, icon, description, temp, humidity, speed);
+      document.querySelector(".city").innerText = "Weather in " + name;
+      document.querySelector(".icon").src = "https://openweathermap.org/img/wn/"+ icon +".png";
+      document.querySelector(".description").innerText = description;
+      document.querySelector(".temp").innerText = temp + "°C";
+      document.querySelector(".humidity").innerText = "Humidity: " + humidity + "%";
+      document.querySelector(".wind").innerText = "Wind speed " + speed + " km/h";
+      //document.querySelector(".weather").classList.remove ("loading");
+      document.body.style.backgroundImage = "url('https://source.unsplash.com/2400x1362?landscapes ,"+ name +"')";
+    },
+    search: function(){
+        this.fetchWeather(document.querySelector(".search-bar").value);
+    },
+};
+
+document.querySelector(".search button")
+.addEventListener("click", function(){
+weather.search();
+});
+
+document.querySelector(".search-bar")
+.addEventListener("keyup", function(event){
+    if(event.key=="Enter")
+    {
+        weather.search();
+    }
+});
+
+// weather.search("delhi");
